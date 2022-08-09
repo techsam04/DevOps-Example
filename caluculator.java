ab76ead4bc763db9c7cae7c74475112a9cadbe1d
techsam04 <samuelsamuels777@gmail.com>
2022-08-09 10:33:19 +0530

    with this file finished my feature branch

---
+++ caluculator.java
@@ -0,0 +1,10 @@
+public class Calculator{
+    public static void main(String[] args{
+        public long sum(int a, int b){
+            return a+b;
+        }
+        public long diff (long a, long b){
+            return a-b;
+        }
+    })
+}
